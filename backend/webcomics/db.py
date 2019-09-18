05a7b76ca90425045a0eef598c42a8a518d4f6a9
dsabsay <danielrsabsay@gmail.com>
2019-09-17 22:58:16 -0700

    backup-db command works

--- backend/webcomics/db.py
+++ backend/webcomics/db.py
@@ -1,3 +1,7 @@
+import copy
+import datetime
+import os
+import subprocess
 import sqlite3
 
 import click
@@ -29,6 +33,62 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+    close_db()
+
+
+def run_shell_command(args, env=None):
+    '''
+    Convenience wrapper around subprocess.run().
+
+    Prints stdout and stderr of the subprocess to the parent's stdout and
+    stderr. Checks return code, raising exception on non-zero exit code.
+    '''
+    cp = subprocess.run(args, check=False, capture_output=True, env=env)
+    click.echo(cp.stdout)
+    click.echo(cp.stderr, err=True)
+
+    cp.check_returncode()
+
+
+def backup_db(isManual):
+    '''
+    Performs the database backup, pushing to the configured repo
+    on GitHub.
+
+    Params:
+        isManual (boolean) - True if invoked manually. False otherwise.
+    '''
+    local_repo = current_app.config['DB_BACKUP_LOCAL_REPO']
+    backup_filename = 'webcomics_backup.sqlite'
+    backup_path = os.path.join(
+        local_repo,
+        backup_filename
+    )
+
+    backup_db = sqlite3.connect(
+        os.path.join(local_repo, backup_filename),
+        detect_types=sqlite3.PARSE_DECLTYPES
+    )
+    live_db = get_db()
+    live_db.backup(backup_db, pages=0, name='main')
+    close_db()  # closes the live db
+    backup_db.close()
+
+    cwd = os.getcwd()
+    os.chdir(local_repo)
+    run_shell_command(['git', 'add', backup_filename])
+
+    commit_time = datetime.datetime.now(tz=datetime.timezone.utc)
+    commit_msg = f'{"Manual" if isManual else "Scheduled"} DB backup on {commit_time} UTC'
+    run_shell_command(['git', 'commit', '-m', commit_msg])
+
+    deploy_key_path = current_app.config['DB_BACKUP_DEPLOY_KEY']
+    env = copy.deepcopy(os.environ)
+    env.update({ 'GIT_SSH_COMMAND': f'ssh -i {deploy_key_path}' })
+    run_shell_command(['git', 'push', 'origin', 'master'], env)
+
+    os.chdir(cwd)
+
 
 @click.command('init-db')
 @flask.cli.with_appcontext
@@ -37,9 +97,28 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 
+@click.command('backup-db')
+@flask.cli.with_appcontext
+def backup_db_command():
+    '''
+    Creates a backup of the database, writing it to a file in a Git
+    repository, comitting it, and pushing the changes to the `origin`
+    remote. The repository must already exist and the remote must be
+    configured.
+
+    This relies on the following configuration items:
+        DB_BACKUP_LOCAL_REPO - path to local Git repository for backups
+        DB_BACKUP_REMOTE_REPO - URL of remote repository for DB backups
+        DB_BACKUP_DEPLOY_KEY - path to SSH deploy key for DB backup repo on GitHub
+    '''
+    backup_db(True)
+    click.echo('Backup succeeded.')
+
+
 def init_app(app):
     """ Register DB functions with application instance. """
     # close db connection after response is returned
     app.teardown_appcontext(close_db)
 
     app.cli.add_command(init_db_command)
+    app.cli.add_command(backup_db_command)
