0e9e87733a77fb02d6f660864a0c3427ca91a7c2
Daniel Sabsay <danielrsabsay@gmail.com>
2019-09-25 06:37:23 +0000

    improve redeploy.sh

--- backend/webcomics/redeploy.sh
+++ backend/webcomics/redeploy.sh
@@ -1,19 +1,27 @@
 #! /bin/bash
 
+set -e
+
 # Kills existing gunicorn processes.
 # Uninstalls webcomics from virtualenv
 # Reinstalls webcomics from master branch
 # Restarts gunicorn
 
+pidfile='~/webcomics/gunicorn.pid'
+
 cd "${0%/*}"  # Change into directory of script
 
-if [ -f 'gunicorn.pid' ]; then
+if [ -f "$pidfile" ]; then
+    echo '### Killing existing gunicorn processes...'
     kill "$(cat gunicorn.pid)"
 fi
 
-pip uninstall webcomics
+echo '### Uninstalling webcomics...'
+pip uninstall --yes webcomics
+echo '### Installing webcomics...'
 pip install --upgrade 'git+https://github.com/dsabsay/webcomics.git#egg=webcomics&subdirectory=backend'
 
-gunicorn --bind=127.0.0.1:4020 --workers=2 --daemon --pidfile=gunicorn.pid 'webcomics:create_app()'
+echo '### Starting gunicorn...'
+gunicorn --bind=127.0.0.1:4020 --workers=2 --daemon --pid="$pidfile" 'webcomics:create_app()'
 
 echo 'Done.'
