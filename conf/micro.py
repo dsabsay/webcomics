638be2d2c957d21c002fd65f99bd22eab308af23
dsabsay <danielrsabsay@gmail.com>
2019-09-28 23:34:48 -0700

    change backup interval to 1 day

--- conf/micro.py
+++ conf/micro.py
@@ -10,7 +10,7 @@ DB_BACKUP_LOCAL_REPO = os.path.join(current_app.instance_path, 'db_backup')
 DB_BACKUP_REMOTE_REPO = 'git@github.com:dsabsay/webcomics-db-backup.git'
 DB_BACKUP_DEPLOY_KEY = os.path.join(current_app.instance_path, 'db_backup_deploy_key_rsa')
 DB_BACKUP_INTERVAL = 1
-DB_BACKUP_TIME_UNIT = 'minutes'
+DB_BACKUP_TIME_UNIT = 'days'
 
 SECRET_KEY = None
 with open(os.path.join(_THIS_DIR, 'flask_secret_key')) as f:
