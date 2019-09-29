32dd57b3e9dc823a9be79e0c59a58d6da7db0a53
dsabsay <danielrsabsay@gmail.com>
2019-09-28 22:34:50 -0700

    fix redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -25,8 +25,8 @@ pip install --upgrade 'git+https://github.com/dsabsay/webcomics.git#egg=webcomic
 cp conf/micro.py "${VIRTUAL_ENV}/var/webcomics-instance/config.py"
 
 # Generate secret key for encrypting session data
-export FLASK_SECRET_KEY=$(python -c 'import os; print(os.urandom(16).hex())')
+flask_secret_key=$(python -c 'import os; print(os.urandom(16).hex())')
 
-gunicorn --bind=127.0.0.1:4020 --workers=2 --daemon --pid=gunicorn.pid 'webcomics:create_app()'
+gunicorn --bind=127.0.0.1:4020 --workers=2 --daemon --pid=gunicorn.pid --env FLASK_SECRET_KEY="${flask_secret_key}" 'webcomics:create_app()'
 
 echo 'Done.'
