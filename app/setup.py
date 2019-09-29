379fbc65e0528a4b3fd582e4c87f7b9e48f1ab58
dsabsay <danielrsabsay@gmail.com>
2019-09-28 21:05:34 -0700

    update setup.py

--- app/setup.py
+++ app/setup.py
@@ -7,11 +7,9 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
+        'flask>=1.1.1',
+        'daemonize>=2.5.0',
+        'psutil>=5.6.3',
+        'schedule>=0.6.0'
     ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'webcomics = webcomics.cli:cli'
-    #     ]
-    # }
 )
